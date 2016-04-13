95d1f0a9993119e91dfab2cbd7babdd2a05bb9e1
Aleks Real <aleksanderreal@gmail.com>
2016-04-13 23:46:47 +0000

    Added create submission functionality by redicrecting to home page

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,9 +1,24 @@
 class WordsController < ApplicationController
   def index
-    @words = Word.order("RANDOM()").first
+    @words = Word.all
   end
 
   def new
     @words = Word.new
   end
+
+  def create
+    @words = Word.create(quote_params)
+    if @words.invalid?
+      flash[:error] = '<strong>Could not save</strong>the data you entered is invalid'
+    end
+    redirect_to root_path 
+  end
+
+  private
+
+  def quote_params
+    params.require(:word).permit(:adjective1, :noun1, :pnoun, :person, :adjective2, :clothing, :noun2, :city)
+  end
+
 end
